f9e1d6904ca1b747f8865111f9d67de8bcb50dd6
lcedRedTea <58790073+erqiu-sj@users.noreply.github.com>
2021-11-05 22:09:32 +0800

    feat: update readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # axiosPackage
+
+## Install
+
+```
+yarn add @zealforchange/axiospackage
+```
+
+or
+
+```
+npm i  @zealforchange/axiospackage
+```
+
+## Function
+
+`更好的状态码处理`
+
+`自动取消重复请求`
--- src/types/statusCodeProcessing.ts
+++ src/types/statusCodeProcessing.ts
@@ -1,8 +1,8 @@
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios'
 // 状态码处理函数
 interface statusCodeProcessingTypes {
-  fn?: (err: AxiosResponse) => void;
-  defaultFn?: (err: AxiosResponse) => void;
+  fn?: (err: AxiosResponse) => void
+  defaultFn?: (err: AxiosResponse) => void
 }
 
-export type statusCodeProcessingCore = Map<number, statusCodeProcessingTypes>;
+export type statusCodeProcessingCore = Map<number, statusCodeProcessingTypes>
